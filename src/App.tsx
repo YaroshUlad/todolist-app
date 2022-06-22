58eebc791afb7176736cfa4dfd458a22565b6dd8
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-22 13:45:14 +0300

    create and check  deleteTDL and updateTitleOfTDL requests

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import {todolistAPI} from "./DAL/todolistAPI";
 
 function App() {
-    /*useEffect(()=>{
-        todolistAPI.createNewToDoList('third tdl')
+    useEffect(()=>{
+        //todolistAPI.updateToDoListTitle('3e8e30e0-393c-4008-a133-65fc4a3ba818', 'firstable')
+        todolistAPI.getToDoLists()
             .then((res)=>{
-                console.log(res.resultCode)
+                console.log(res)
             })
-    }, [])*/
+    }, [])
 
   return (
     <div className="App">
--- src/DAL/todolistAPI.ts
+++ src/DAL/todolistAPI.ts
@@ -14,6 +14,13 @@ export type CreateTDLResponseType = {
     resultCode: number
 }
 
+export type DeleteTDLResponseType = {
+    data: {}
+    fieldsErrors: []
+    messages: []
+    resultCode: number
+}
+
 export const todolistAPI = {
     getToDoLists() {
         return instance.get<ToDoListType[]>('todo-lists')
@@ -23,6 +30,14 @@ export const todolistAPI = {
         const bodyOfRequest = {"title": title}
         return instance.post<CreateTDLResponseType>('todo-lists', bodyOfRequest)
             .then(res => res.data)
-
+    },
+    deleteToDoList (tdlId: string) {
+        return instance.delete<DeleteTDLResponseType>(`todo-lists/${tdlId}`)
+            .then(res=>res.data)
+    },
+    updateToDoListTitle (tdlId: string, title: string) {
+        const bodyOfRequest = {"title": title}
+        return instance.put<DeleteTDLResponseType>(`todo-lists/${tdlId}`,bodyOfRequest)
+            .then(res=>res.data)
     }
 }
