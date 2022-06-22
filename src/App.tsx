81e60cbb91861bebc5161b2b14de0958ad915879
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-22 13:57:26 +0300

    add redux-thunk lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import {todolistAPI} from "./DAL/todolistAPI";
+
 
 function App() {
-    useEffect(()=>{
-        //todolistAPI.updateToDoListTitle('3e8e30e0-393c-4008-a133-65fc4a3ba818', 'firstable')
-        todolistAPI.getToDoLists()
-            .then((res)=>{
-                console.log(res)
-            })
-    }, [])
+
 
   return (
     <div className="App">
