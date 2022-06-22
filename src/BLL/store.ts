6a045b9259aa88a3614ac4a306596d1d20192aff
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-22 17:24:31 +0300

    add tasksReducer to rootReducer

--- src/BLL/store.ts
+++ src/BLL/store.ts
@@ -1,9 +1,11 @@
 import {combineReducers, legacy_createStore as createStore} from "redux";
 import {toDoListsReducer} from "./toDoListsReducer";
+import {taskReducer} from "./tasksReducer";
 
 
 const rootReducer = combineReducers({
     toDoLists: toDoListsReducer,
+    tasks: taskReducer,
 })
 export type AppStoreType = ReturnType<typeof rootReducer>
 
