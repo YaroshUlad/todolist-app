eef0d83128a32a50e98400fcb40c123c59bb1924
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-22 15:55:25 +0300

    create createTask, deleteTask, updateTask requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import {tasksAPI} from "./DAL/tasksAPI";
-import {todolistAPI} from "./DAL/todolistAPI";
+
 
 
 function App() {
     useEffect(() => {
         /*todolistAPI.getToDoLists()
             .then(console.log)*/
-        tasksAPI.getTasks("6e913c7e-ece9-490c-94cd-aadcc5cc55bb")
-            .then(console.log)
+       // tasksAPI.getTasks("6e913c7e-ece9-490c-94cd-aadcc5cc55bb")
+           // .then(res=> console.log(res))
     }, [])
 
     return (
---
+++ src/BLL/tasksReducer.ts
@@ -0,0 +1,7 @@
+
+export enum ACTIONS_TYPES {
+    setTasks = 'SET_TASKS_TO_STATE',
+    deleteTask = 'DELETE_TASK_FROM_STATE',
+    updateTask = 'UPDATE_TASK',
+    createNewTask = 'CREATE_NEW_TASK',
+}
