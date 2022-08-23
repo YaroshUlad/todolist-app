d76f01b161f7484ec45efc923897d704230f9f86
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-08-23 15:25:31 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './app/App';
-import {Provider} from 'react-redux';
-import {store} from './app/store';
-import {BrowserRouter} from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+
+import { store } from 'app/store';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,22 +1,29 @@
-import {
-	setAppErrorAC,
-	SetAppErrorActionType,
-	setAppStatusAC,
-	SetAppStatusActionType
-} from '../app/app-reducer';
-import {ResponseType} from '../api/todolists-api';
-import {Dispatch} from 'redux';
+import { Dispatch } from 'redux';
 
-export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
-	if (data.messages.length) {
-		dispatch(setAppErrorAC(data.messages[0]));
-	} else {
-		dispatch(setAppErrorAC('Some error occurred'));
-	}
-	dispatch(setAppStatusAC('failed'));
+import { ResponseType } from '../api/todolists-api';
+import {
+  setAppErrorAC,
+  SetAppErrorActionType,
+  setAppStatusAC,
+  SetAppStatusActionType,
+} from '../app/app-reducer';
+
+export const handleServerAppError = <D>(
+  data: ResponseType<D>,
+  dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>,
+): void => {
+  if (data.messages.length) {
+    dispatch(setAppErrorAC(data.messages[0]));
+  } else {
+    dispatch(setAppErrorAC('Some error occurred'));
+  }
+  dispatch(setAppStatusAC('failed'));
 };
 
-export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
-	dispatch(setAppErrorAC(error.message ? error.message : 'Some error occurred'));
-	dispatch(setAppStatusAC('failed'));
+export const handleServerNetworkError = (
+  error: { message: string },
+  dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>,
+): void => {
+  dispatch(setAppErrorAC(error.message ? error.message : 'Some error occurred'));
+  dispatch(setAppStatusAC('failed'));
 };
