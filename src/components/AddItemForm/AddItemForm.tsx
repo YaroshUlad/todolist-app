3948e6f65f5f37927dda00f086407a4e42dba876
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-08-23 14:57:19 +0300

    refactor

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,56 +1,60 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
-import TextField from '@mui/material/TextField';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
+
+import { AddBox } from '@mui/icons-material';
 import IconButton from '@mui/material/IconButton';
-import {AddBox} from '@mui/icons-material';
+import TextField from '@mui/material/TextField';
 
 type AddItemFormPropsType = {
-	addItem: (title: string) => void
-	disabled?: boolean
-}
+  addItem: (title: string) => void;
+  disabled?: boolean;
+};
 
 export const AddItemForm = React.memo(function ({
-	                                                addItem,
-	                                                disabled = false
-                                                }: AddItemFormPropsType) {
-	console.log('AddItemForm called');
+  addItem,
+  disabled = false,
+}: AddItemFormPropsType) {
+  console.log('AddItemForm called');
 
-	let [title, setTitle] = useState('');
-	let [error, setError] = useState<string | null>(null);
+  let [title, setTitle] = useState('');
+  let [error, setError] = useState<string | null>(null);
 
-	const addItemHandler = () => {
-		if (title.trim() !== '') {
-			addItem(title);
-			setTitle('');
-		} else {
-			setError('Title is required');
-		}
-	};
+  const addItemHandler = (): void => {
+    if (title.trim() !== '') {
+      addItem(title);
+      setTitle('');
+    } else {
+      setError('Title is required');
+    }
+  };
 
-	const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-		setTitle(e.currentTarget.value);
-	};
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+    setTitle(e.currentTarget.value);
+  };
 
-	const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-		if (error !== null) {
-			setError(null);
-		}
-		if (e.charCode === 13) {
-			addItemHandler();
-		}
-	};
+  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>): void => {
+    if (error !== null) {
+      setError(null);
+    }
+    if (e.charCode === 13) {
+      addItemHandler();
+    }
+  };
 
-	return <div>
-		<TextField variant="outlined"
-		           disabled={disabled}
-		           error={ !!error}
-		           value={title}
-		           onChange={onChangeHandler}
-		           onKeyPress={onKeyPressHandler}
-		           label="Title"
-		           helperText={error}
-		/>
-		<IconButton color="primary" onClick={addItemHandler} disabled={disabled}>
-			<AddBox/>
-		</IconButton>
-	</div>;
+  return (
+    <div>
+      <TextField
+        variant="outlined"
+        disabled={disabled}
+        error={!!error}
+        value={title}
+        onChange={onChangeHandler}
+        onKeyPress={onKeyPressHandler}
+        label="Title"
+        helperText={error}
+      />
+      <IconButton color="primary" onClick={addItemHandler} disabled={disabled}>
+        <AddBox />
+      </IconButton>
+    </div>
+  );
 });
--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,35 +1,36 @@
-import React from 'react';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert, {AlertProps} from '@mui/material/Alert';
-import {useDispatch, useSelector} from 'react-redux';
-import {AppRootStateType} from '../../app/store';
-import {setAppErrorAC} from '../../app/app-reducer';
-import {SnackbarCloseReason} from '@mui/material/Snackbar/Snackbar';
+import React, { ReactElement } from 'react';
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-	props, ref) {
-	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setAppErrorAC } from 'app/app-reducer';
+import { AppRootStateType } from 'app/store';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+export const ErrorSnackbar = (): ReactElement => {
+  const error = useSelector<AppRootStateType, string | null>(state => state.app.error);
 
-export function ErrorSnackbar() {
+  const dispatch = useDispatch();
 
-	const error = useSelector<AppRootStateType, string | null>(state => state.app.error);
+  const handleClose = (
+    event?: React.SyntheticEvent<any> | Event,
+    reason?: SnackbarCloseReason,
+  ): void => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    dispatch(setAppErrorAC(null));
+  };
 
-	const dispatch = useDispatch();
-
-	const handleClose = (event?: React.SyntheticEvent<any> | Event, reason?: SnackbarCloseReason) => {
-		if (reason === 'clickaway') {
-			return;
-		}
-		dispatch(setAppErrorAC(null));
-	};
-
-	return (
-		<Snackbar open={error !== null} autoHideDuration={6000} onClose={handleClose}>
-			<Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
-				{error}
-			</Alert>
-		</Snackbar>
-	);
-}
+  return (
+    <Snackbar open={error !== null} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+        {error}
+      </Alert>
+    </Snackbar>
+  );
+};
